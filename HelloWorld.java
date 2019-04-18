2083ff66e265ac60b8abcaf97f779681eae1466f
afnaank <afnaankarimi@hotmail.com>
2019-04-18 01:13:36 -0400

    Add HelloWorld program

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Simple java program that prints Hello World
+ */
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
